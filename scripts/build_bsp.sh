a30eac81bf9572eb5631d54dcfb94f8c45534ccf
Sai Sree Kartheek Adivi <sskartheek.adivi@outlook.com>
2023-05-30 21:49:36 +0530

    code refactoring and mmdebstrap umount bug fix
    - The current scripts use .ini config file which looks like a windows
      file. So switch to .toml
    
    - Split the config file into 3
        - builds
        - machines
        - bsp_sources
    
    - new build variants can be added in builds.toml and enabled in the
      [builds] array. This way, someone can create new config without
      messing up a working config
    
    - Update readme to install toml-cli python3 module which is needed to
      parse toml files
    
    - Add a new config for Bookworm builds
    
    - Add Debian 12 (Bookworm) configuration
    
    - Fix occasional build failures which says can't umount /dev/proc by
      adding a custom hook to sleep for 10 secs before performing umount.
    
    - Add a .gitignore for build and tools directory to avoid accidentally
      pushing build and tools dirs
    
    - Update Build.yml workflow
    
    Signed-off-by: Sai Sree Kartheek Adivi <sskartheek.adivi@outlook.com>

--- scripts/build_bsp.sh
+++ scripts/build_bsp.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-function setup_bsp_build() {
-machine=$1
+function build_bsp() {
+build=$1
+machine=$2
+bsp_version=$3
 
-    mkdir -p ${topdir}/build/bsp_sources; cd ${topdir}/build/bsp_sources
+    setup_bsp_build ${build} ${machine} ${bsp_version}
+    build_atf $machine
+    build_optee $machine
+    build_uboot $machine
+}
+
+
+function setup_bsp_build() {
+build=$1
+machine=$2
+bsp_version=$3
+
+    mkdir -p ${topdir}/build/${build}/bsp_sources; cd ${topdir}/build/${build}/bsp_sources
 
     echo "> BSP sources: checking .."
     if [ ! -d core-secdev-k3 ]; then
@@ -16,12 +30,12 @@ machine=$1
     else
         echo ">> core-secdev-k3: available"
     fi
-    export TI_SECURE_DEV_PKG=${topdir}/build/bsp_sources/core-secdev-k3
+    TI_SECURE_DEV_PKG=${topdir}/build/${build}/bsp_sources/core-secdev-k3
 
     if [ ! -d trusted-firmware-a ]; then
-        cd ${topdir}/build/bsp_sources
+        cd ${topdir}/build/${build}/bsp_sources
         echo ">> atf: not found. cloning .."
-        atf_srcrev=`read_machine_config ${machine} atf_srcrev`
+        atf_srcrev=($(read_bsp_config ${bsp_version} atf_srcrev))
 
         git clone https://git.trustedfirmware.org/TF-A/trusted-firmware-a.git
 
@@ -32,12 +46,12 @@ machine=$1
     else
         echo ">> core-secdev-k3: available"
     fi
-    export TFA_DIR=${topdir}/build/bsp_sources/trusted-firmware-a
+    TFA_DIR=${topdir}/build/${build}/bsp_sources/trusted-firmware-a
 
     if [ ! -d optee_os ]; then
-        cd ${topdir}/build/bsp_sources
+        cd ${topdir}/build/${build}/bsp_sources
         echo ">> optee_os: not found. cloning .."
-        optee_srcrev=`read_machine_config ${machine} optee_srcrev`
+        optee_srcrev=($(read_bsp_config ${bsp_version} optee_srcrev))
 
         git clone https://github.com/OP-TEE/optee_os.git
 
@@ -48,12 +62,12 @@ machine=$1
     else
         echo ">> optee_os: available"
     fi
-    export OPTEE_DIR=${topdir}/build/bsp_sources/optee_os
+    OPTEE_DIR=${topdir}/build/${build}/bsp_sources/optee_os
 
     if [ ! -d ti-u-boot ]; then
-        cd ${topdir}/build/bsp_sources
+        cd ${topdir}/build/${build}/bsp_sources
         echo ">> ti-u-boot: not found. cloning .."
-        uboot_srcrev=`read_machine_config ${machine} uboot_srcrev`
+        uboot_srcrev=($(read_bsp_config ${bsp_version} uboot_srcrev))
         git clone \
             https://git.ti.com/git/ti-u-boot/ti-u-boot.git \
             -b ${uboot_srcrev} \
@@ -69,12 +83,12 @@ machine=$1
     else
         echo ">> ti-u-boot: available"
     fi
-    export UBOOT_DIR=${topdir}/build/bsp_sources/ti-u-boot
+    UBOOT_DIR=${topdir}/build/${build}/bsp_sources/ti-u-boot
 
     if [ ! -d k3-image-gen ]; then
-        cd ${topdir}/build/bsp_sources
+        cd ${topdir}/build/${build}/bsp_sources
         echo ">> k3-image-gen: not found. cloning .."
-        k3ig_srcrev=`read_machine_config ${machine} k3ig_srcrev`
+        k3ig_srcrev=($(read_bsp_config ${bsp_version} k3ig_srcrev))
         git clone \
             https://git.ti.com/git/k3-image-gen/k3-image-gen.git \
             -b ${k3ig_srcrev} \
@@ -84,12 +98,12 @@ machine=$1
     else
         echo ">> k3-image-gen: available"
     fi
-    export K3IG_DIR=${topdir}/build/bsp_sources/k3-image-gen
+    K3IG_DIR=${topdir}/build/${build}/bsp_sources/k3-image-gen
 
     if [ ! -d ti-linux-firmware ]; then
-        cd ${topdir}/build/bsp_sources
+        cd ${topdir}/build/${build}/bsp_sources
         echo ">> ti-linux-firmware: not found. cloning .."
-        linux_fw_srcrev=`read_machine_config ${machine} linux_fw_srcrev`
+        linux_fw_srcrev=($(read_bsp_config ${bsp_version} linux_fw_srcrev))
         git clone \
             https://git.ti.com/git/processor-firmware/ti-linux-firmware.git \
             -b ${linux_fw_srcrev} \
@@ -99,14 +113,14 @@ machine=$1
     else
         echo ">> ti-linux-firmware: available"
     fi
-    dmfw_machine=`read_machine_config ${machine} dmfw_machine`
-    export SYSFW_DIR=${topdir}/build/bsp_sources/ti-linux-firmware/ti-sysfw
-    export DMFW_DIR=${topdir}/build/bsp_sources/ti-linux-firmware/ti-dm/${dmfw_machine}
+    dmfw_machine=($(read_machine_config ${machine} dmfw_machine))
+    SYSFW_DIR=${topdir}/build/${build}/bsp_sources/ti-linux-firmware/ti-sysfw
+    DMFW_DIR=${topdir}/build/${build}/bsp_sources/ti-linux-firmware/ti-dm/${dmfw_machine}
 
     if [ ! -d ti-linux-kernel ]; then
-        cd ${topdir}/build/bsp_sources
+        cd ${topdir}/build/${build}/bsp_sources
         echo ">> ti-linux-kernel: not found. cloning .."
-        linux_kernel_srcrev=`read_machine_config ${machine} linux_kernel_srcrev`
+        linux_kernel_srcrev=($(read_bsp_config ${bsp_version} linux_kernel_srcrev))
         git clone \
             https://git.ti.com/git/ti-linux-kernel/ti-linux-kernel.git \
             -b ${linux_kernel_srcrev} \
@@ -122,12 +136,12 @@ machine=$1
     else
         echo ">> ti-linux-kernel: available"
     fi
-    export KERNEL_DIR=${topdir}/build/bsp_sources/ti-linux-kernel
+    KERNEL_DIR=${topdir}/build/${build}/bsp_sources/ti-linux-kernel
 
     if [ ! -d ti-img-rogue-driver ]; then
-        cd ${topdir}/build/bsp_sources
+        cd ${topdir}/build/${build}/bsp_sources
         echo ">> ti-img-rogue-driver: not found. cloning .."
-        img_rogue_driver_srcrev=`read_machine_config ${machine} img_rogue_driver_srcrev`
+        img_rogue_driver_srcrev=($(read_bsp_config ${bsp_version} img_rogue_driver_srcrev))
         git clone \
             https://git.ti.com/git/graphics/ti-img-rogue-driver.git \
             -b ${img_rogue_driver_srcrev} \
@@ -143,11 +157,11 @@ machine=$1
     else
         echo ">> ti-img-rogue-driver: available"
     fi
-    export IMG_ROGUE_DRIVER_DIR=${topdir}/build/bsp_sources/ti-img-rogue-driver
+    IMG_ROGUE_DRIVER_DIR=${topdir}/build/${build}/bsp_sources/ti-img-rogue-driver
 
     echo "> BSP sources: cloned"
     echo "> BSP sources: creating backup .."
-    cd ${topdir}/build
+    cd ${topdir}/build/${build}
     tar --use-compress-program="pigz --best --recursive | pv" -cf bsp_sources.tar.xz bsp_sources
     echo "> BSP sources: backup created .."
 
@@ -158,7 +172,7 @@ function build_atf() {
 machine=$1
 
     cd $TFA_DIR
-    target_board=`read_machine_config ${machine} atf_target_board`
+    target_board=($(read_machine_config ${machine} atf_target_board))
 
     echo "> ATF: building .."
     make -j`nproc` ARCH=aarch64 CROSS_COMPILE=aarch64-none-linux-gnu- PLAT=k3 TARGET_BOARD=${target_board} SPD=opteed
@@ -171,7 +185,7 @@ function build_optee() {
 machine=$1
 
     cd ${OPTEE_DIR}
-    platform=`read_machine_config ${machine} optee_platform`
+    platform=($(read_machine_config ${machine} optee_platform))
 
     echo "> optee: building .."
     make -j`nproc` CROSS_COMPILE64=aarch64-none-linux-gnu- CROSS_COMPILE=arm-none-linux-gnueabihf- PLATFORM=${platform} CFG_ARM64_core=y
@@ -183,9 +197,9 @@ machine=$1
 function build_uboot() {
 machine=$1
 
-    uboot_r5_defconfig=`read_machine_config ${machine} uboot_r5_defconfig`
-    uboot_a53_defconfig=`read_machine_config ${machine} uboot_a53_defconfig`
-    sysfw_soc=`read_machine_config ${machine} sysfw_soc`
+    uboot_r5_defconfig=($(read_machine_config ${machine} uboot_r5_defconfig))
+    uboot_a53_defconfig=($(read_machine_config ${machine} uboot_a53_defconfig))
+    sysfw_soc=($(read_machine_config ${machine} sysfw_soc))
 
     echo "> dmfw: signing .."
     ${TI_SECURE_DEV_PKG}/scripts/secure-binary-image.sh ${DMFW_DIR}/ipc_echo_testb_mcu1_0_release_strip.xer5f ${DMFW_DIR}/ipc_echo_testb_mcu1_0_release_strip.xer5f.signed
@@ -197,7 +211,7 @@ machine=$1
 
     cd ${K3IG_DIR}
     make -j`nproc` ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabihf- SOC=${sysfw_soc} SOC_TYPE=hs-fs SBL=${UBOOT_DIR}/out/r5/spl/u-boot-spl.bin SYSFW_DIR=${SYSFW_DIR}
-    cp ${K3IG_DIR}/tiboot3.bin ${topdir}/build/boot_${machine}/
+    cp ${K3IG_DIR}/tiboot3.bin ${topdir}/build/${build}/tisdk-${distro}-${machine}-boot/
     # TODO: Also build for GP and HS
     # make -j`nproc` ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabihf- SOC=${sysfw_soc} SOC_TYPE=gp SBL=${UBOOT_DIR}/out/r5/spl/u-boot-spl.bin SYSFW_DIR=${SYSFW_DIR}
     # make -j`nproc` ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabihf- SOC=${sysfw_soc} SOC_TYPE=hs SBL=${UBOOT_DIR}/out/r5/spl/u-boot-spl.bin SYSFW_DIR=${SYSFW_DIR}
@@ -206,8 +220,8 @@ machine=$1
     echo "> uboot-a53: building .."
     make -j`nproc` ARCH=arm CROSS_COMPILE=aarch64-none-linux-gnu- ${uboot_a53_defconfig} O=${UBOOT_DIR}/out/a53
     make -j`nproc` ARCH=arm CROSS_COMPILE=aarch64-none-linux-gnu- ATF=${TFA_DIR}/build/k3/lite/release/bl31.bin.signed TEE=${OPTEE_DIR}/out/arm-plat-k3/core/tee-pager_v2.bin.signed DM=${DMFW_DIR}/ipc_echo_testb_mcu1_0_release_strip.xer5f.signed O=${UBOOT_DIR}/out/a53
-    cp ${UBOOT_DIR}/out/a53/tispl.bin ${topdir}/build/boot_${machine}/
-    cp ${UBOOT_DIR}/out/a53/u-boot.img ${topdir}/build/boot_${machine}/
+    cp ${UBOOT_DIR}/out/a53/tispl.bin ${topdir}/build/${build}/tisdk-${distro}-${machine}-boot/
+    cp ${UBOOT_DIR}/out/a53/u-boot.img ${topdir}/build/${build}/tisdk-${distro}-${machine}-boot/
 }
 
 function build_kernel() {
@@ -244,8 +258,8 @@ machine=$1
 rootfs_dir=$2
 kernel_dir=$3
 
-    pvr_target=`read_machine_config ${machine} pvr_target`
-    pvr_window_system=`read_machine_config ${machine} pvr_window_system`
+    pvr_target=($(read_machine_config ${machine} pvr_target))
+    pvr_window_system=($(read_machine_config ${machine} pvr_window_system))
     cd ${IMG_ROGUE_DRIVER_DIR}
 
     echo "ti-img-rogue-driver: building .."
@@ -254,5 +268,4 @@ kernel_dir=$3
     echo "ti-img-rogue-driver: installing .."
     cd binary_am62_linux_wayland_release/target_aarch64/kbuild
     make -C ${kernel_dir} ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- INSTALL_MOD_PATH=${rootfs_dir} INSTALL_MOD_STRIP=1 M=`pwd` modules_install
-
 }
---
+++ scripts/build_distro.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+function generate_rootfs() {
+build=$1
+machine=$2
+distro=$3
+
+    hostname=($(read_machine_config ${machine} hostname))
+    bdebstrap \
+        -c ${topdir}/configs/bdebstrap_configs/${distro}.yaml \
+        --name ${topdir}/build/${build} \
+        --target tisdk-${distro}-${machine}-rootfs \
+        --hostname "${hostname}" -f
+
+    cd ${topdir}/build/
+    echo ">> mmdebstrap has issues with unmounting /proc and /dev/pts, etc. Umounting them manually .."
+    umount ./${build}/tisdk-${distro}-${machine}-rootfs/*
+    umount ./${build}/tisdk-${distro}-${machine}-rootfs/dev/*
+    umount ./${build}/tisdk-${distro}-${machine}-rootfs/*
+    umount ./${build}/tisdk-${distro}-${machine}-rootfs/dev/*
+
+    ROOTFS_DIR=${topdir}/build/${build}/tisdk-${distro}-${machine}-rootfs
+
+}
+
+function package_and_clean() {
+build=$1
+    
+    cd ${topdir}/build/${build}
+
+    echo "> Cleaning up ${build}"
+    tar --use-compress-program="pigz --best --recursive | pv" -cf tisdk-${distro}-${machine}-rootfs.tar.xz tisdk-${distro}-${machine}-rootfs
+    rm -rf tisdk-${distro}-${machine}-rootfs
+
+    tar --use-compress-program="pigz --best --recursive | pv" -cf tisdk-${distro}-${machine}-boot.tar.xz tisdk-${distro}-${machine}-boot
+    rm -rf tisdk-${distro}-${machine}-boot
+
+    rm -rf bsp_sources
+
+    cd ${topdir}/build/
+
+    echo "> Packaging ${build}"
+    tar --use-compress-program="pigz --best --recursive | pv" -cf ${build}.tar.xz ${build}
+}
+
